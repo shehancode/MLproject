dda366e2deefc1c14f5e10739822283f610e32f1
shehancode <shehan1349@gmail.com>
2024-01-19 08:30:26 +0530

    Hyper Parameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -47,7 +47,39 @@ class ModelTrainer:
                 "AdaBoost Classifier": AdaBoostRegressor()
             }
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            params={
+                "Random Forest": {
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson']
+                },
+                "Gradient Boosting": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression": {},
+                "K-Neighbours Classifier": {
+                    'n_neighbors': [5,7,9,11]
+                },
+                "XGBClassifier": {
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Classifier": {
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01,0.05,0.1],
+                    'iterations': [30,50,100]
+                },
+                "AdaBoost Classifier": {
+                    'learning_rate': [.1,.01,.5,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                              models=models,param=params)
 
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
